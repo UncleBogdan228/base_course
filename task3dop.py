9c2640050b5153abbf6884c19dd9191f48b467bd
UncleBogdan228 <oduch200608@gmail.com>
2024-10-09 20:14:24 +0000

    дз

---
+++ task3dop.py
@@ -0,0 +1,9 @@
+a = int(input('введите число:'))
+y = str(a)
+z = "" 
+
+for i in range(len(y) - 1, -1, -1):
+    z = z + y[i]
+print(z)
+a = int(z)
+
---
+++ task4.py
@@ -0,0 +1,15 @@
+num = int(input('Введите количество членов:'))
+q1 = 1 
+q2 = 0
+t = 0
+for i in range (0, num):
+    q3 = q1 + q2
+    print( q3 )
+    t = q2
+    q1 = t
+    q2 = q3
+    
+
+
+
+#1 1 2 3 5 8 
