add9591f6f74458b944336aaceab953df5f95100
UncleBogdan228 <oduch200608@gmail.com>
2024-10-22 19:45:40 +0000

    Лекия 3

--- task2.py
+++ task2.py
@@ -1,9 +1,7 @@
 import numpy as np
-import constant 
+from constant import g
 h = 100
 a = 45
 b  = 35
-g = constant.g
-
-v = (g*h*np.tan(b)**2 / ((2*(np.cos(a)**2)) * (1 - np.tan(b) * np.tan(a))))**0.5
+v = ((g*h*np.tan(b)**2) / (2*np.cos(a)**2 * (1 - np.tan(b) * np.tan(a))))**0.5
 print(v)
