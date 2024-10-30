dc0be0cc2ba055a232f6ec2c8c26680188c96202
UncleBogdan228 <oduch200608@gmail.com>
2024-10-30 15:20:53 +0000

    лек3

--- task3.py
+++ task3.py
@@ -1,13 +1,16 @@
 import numpy as np
 from constant import g
-t = 5
-xnull = int(input())
-ynull= int(input())
-vnull= int(input())
-x = xnull + vnull*t
-y = ynull + vnull*t - (g*t**2)/2
-mass = np.zeros ((1,3))
-mass[0,0] = t
-mass[0,1] = x
-mass[0,2] = y
-print(mass)
+#t = int(input('Введите начальное время от 0 до 5 секунд:'))
+xnull = int(input('Введите начальную  координату по х:'))
+ynull= int(input('Введите начальную координату по у:'))
+vnull= int(input('Введите начальную скорость:'))
+for i in np.linspace(0,5,100):
+    t = i
+    x = xnull + vnull*t
+    y = ynull + vnull*t - (g*t**2)/2
+    mass = np.zeros ((1,3))
+    mass[0,0] = t
+    mass[0,1] = x
+    mass[0,2] = y
+    print(mass)
+
--- task4.py
+++ task4.py
@@ -5,7 +5,7 @@ mass  = np.zeros((n,m))
 for i in range (0,n):
     for j in range (0,m):
         elem = np.sin(n * i + m * j + 1)
-        if elem >= 0:
+        if elem > 0:
             mass[i,j] = elem
         else: 
             mass[i,j]=0
